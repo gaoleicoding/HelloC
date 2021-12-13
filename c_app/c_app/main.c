3af789e830722c3eedd8cd3f55e30c83ef54b3b2
gaolei <756985063@qq.com>
2021-12-13 15:00:16 +0800

    feat(Project):1、编写HelloWorld

---
+++ c_app/c_app/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c_app
+//
+//  Created by gaolei on 2021/12/13.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
